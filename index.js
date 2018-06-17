d7af9aa726e2998d19d9ef14e37442661c3ac074
MizoPro <hamza.randy@gmail.com>
2018-06-17 23:49:15 +0100

    Regexp routes

--- index.js
+++ index.js
@@ -18,12 +18,19 @@ const path = require("path");
  * Local Database
  */
 
-const animedb = require("./db/anime.json");
+const db = require("./lib/db.js");
+const animedb = new db.AnimeCollection( ...require("./db/anime.json").sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase())) );
 //const characterdb = require("./data/character.json");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const config = {
+
+	TITLES_PER_PAGE: 50
+
+};
 
 /*
  * App metadata
@@ -46,34 +53,67 @@ app
 
 	.get('/', (req, res) => res.render("pages/index", {req: req}))
 
-	.get('about', (req, res) => res.render("pages/about", {req: req}))
-	.get('author', (req, res) => res.render("pages/author", {req: req}))
+	.get('/about', (req, res) => res.render("pages/about", {req: req}))
+	.get('/author', (req, res) => res.render("pages/author", {req: req}))
 
-	.get('/a/', (req, res) => res.render("pages/animelist", {req: req}))
-	.get('/a/all', (req, res) => res.render("pages/anime_all", {req: req}))
-	.get('/a/random', (req, res) => res.redirect(`/a/${randomAnimeID()}`))
+	.get('/a/', (req, res) => res.render("pages/anime_index", {req: req}))
+	.get(/^\/a\/all(?:\/(tv|ova|movie|ona|special|music))?$/i, (req, res) =>
+		res.render("pages/anime_all", {
+			req: req,
+			animedb: (req.params[0]) ? animedb.filterByType(req.params[0]) : animedb, 
+			pagination: {}
+		})
+	)
+
+	.get(/^\/a\/top(?:\/(tv|ova|movie|ona|special|music))?$/i, (req, res) => {
+		let page = (!isNaN(req.query.page) && req.query.page > 0) ? req.query.page * 1 : 1,
+			end = page * config.TITLES_PER_PAGE, 
+			start = end - config.TITLES_PER_PAGE;
+		let sorted = (req.params[0] ? animedb.filterByType(req.params[0]) : animedb)
+			.sortByScore();
+		let sliced = sorted.slice(start, end);
+		let total = Math.floor(sorted.length / config.TITLES_PER_PAGE), 
+			next = (page !== total) ? page + 1 : null, 
+			prev = (page !== 1) ? page - 1 : null;
+		
+		res.render("pages/anime_all", {
+			req: req,
+			animedb: sliced, 
+			pagination: {
+				current: page, 
+				total: total, 
+				prev: prev, 
+				next: next
+			}
+		})
+	})
+
+	.get(/^\/a\/random(?:\/(tv|ova|movie|ona|special|music))?/i, (req, res) => 
+		res.redirect(`/a/${animedb.getRandomId((req.params[0]) ? function (anime) {
+			return anime.type.toLowerCase() === req.params[0].toLowerCase();
+		} : null)}`)
+	)
+
 	.get('/a/:id', (req, res) => {
-		const a = animedb.filter(a => a.id == req.params.id)[0];
-		(typeof a !== "undefined") ?
+		const a = animedb.findById(req.params.id);
+		(a) ?
 			res.render("pages/anime", {anime: a, req: req}) :
-			res.sendStatus(404);
+			render404(req, res);
 	})
 
-	//.get('/c/', (req, res) => res.send("Error: Characters database is NOT implemented yet!"))
+	//.get('/c/', (req, res) => res.status(404).send("Characters database is NOT implemented yet!"))
 
-	.use((req, res, next) => {
-		res.status(404).render("pages/404", {req: req})
-	})
+	.use(render404)
 
 	.listen(PORT, () => console.log(`AnimeList -- Listening on ${PORT}`));
 
 
 /**
- * Get a random anime id from `animedb`
+ * Render 404 Page
  * 
  * @returns {Number}
  */
-function randomAnimeID ()
+function render404 (req, res)
 {
-	return animedb[Math.floor(Math.random() * animedb.length)].id;
+	res.status(404).render("pages/404", {req: req});
 }
